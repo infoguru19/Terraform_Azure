d754c4b33102ae3b1fa268f05e221781b150eeb6
Ravi <infoguru19@gmail.com>
2025-12-05 15:34:59 +0530

    boot diag work

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -15,12 +15,12 @@ provider "azurerm" {
   
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "demo" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "demo" {
  
   name                     = "tiyaranjan1607"
   resource_group_name      = azurerm_resource_group.example.name
