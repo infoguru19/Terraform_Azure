f544a0d5f88ecc559cca3e5576ebe859d849415c
Ravi <infoguru19@gmail.com>
2025-12-06 17:05:00 +0530

    Stop tracking backend.tf

--- Day-6/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-      resource_group_name  = "tfstate"
-      storage_account_name = "tfstate10942"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
-  }
-}
